d228aaf2166968679bbede030847c5f4f17b9e66
dujunhui007 <810743626@qq.com>
2017-12-13 18:02:29 +0800

    small change

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 $(document).ready(
-  // $("nav .navRightItems ").addClass("equalThan").removeClass("moreThan")
-$(window).scroll(function () {
+  $('.deScroll').css('overflow', 'hidden'),
+  $(".pageDown").click(function () {
+    $('.deScroll').css('overflow', 'auto')
+  }),
+  $(window).scroll(function () {
     if ($(window).scrollTop() > 0) {
       $("nav").css("background", "#d3edf6");
       $("nav .navRightItems ").addClass("moreThan").removeClass("equalThan");
     } else {
       $("nav").css("background", "#28a5d0");
       $("nav .navRightItems ").addClass("equalThan").removeClass("moreThan");
+      $('.deScroll').css('overflow', 'hidden')
     }
   })
 );
